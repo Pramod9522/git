35e211203e221659d2d8b2f29e693bf4351f2b5f
Pramod M C <pramodmcpramodc@gmail.com>
2026-06-28 10:48:37 +0530

    added Demo.java

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+  public static void main(String[] args) {
+    System.out.print("demo program");
+        System.out.print("demo program1");
+        System.out.print("demo program2");
+
+  }
+
+}
