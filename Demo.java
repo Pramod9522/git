3720835fcab5268c029c28f3d6cdd26b7ac665e4
Pramod M C <pramodmcpramodc@gmail.com>
2026-06-28 10:51:01 +0530

    first commit

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
 public class Demo {
   public static void main(String[] args) {
-    System.out.print("demo program");
         System.out.print("demo program1");
         System.out.print("demo program2");
+            System.out.print("demo program3");
+
 
   }
 
